e7d184fe884b8610cfb47a70ab6d02f310d02fc8
Akinlolu Ogunleye <akinloludavid2727@gmail.com>
2022-03-03 18:50:19 +0100

    added timer

--- js/main.js
+++ js/main.js
@@ -60,11 +60,13 @@ const finaleDate = new Date("March 19, 2022 00:00:00").getTime();
 
 const timer = () => {
   const now = new Date().getTime();
+  const timer = document.getElementById("timer-container");
   let diff = finaleDate - now;
   // Showing the alert when the counter time finishes.
   if (diff < 0) {
     document.querySelector(".alert").style.display = "block";
-    document.querySelector(".container").style.display = "none";
+    timer.classList.remove("d-flex");
+    timer.style.display = "none";
   }
 
   let days = Math.floor(diff / (1000 * 60 * 60 * 24));
